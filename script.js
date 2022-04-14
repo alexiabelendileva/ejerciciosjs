539e571a81b73bb271cbdc108b832f29aab76e6b
belen di leva <alexiabelengonzalezdileva@gmail.com>
2022-04-13 22:58:05 -0300

    ejercicio complementario 1

---
+++ script.js
@@ -0,0 +1,14 @@
+let producto = parseInt(prompt("Ingrese código del producto"))
+	if (producto == 1) {
+console.log("El producto es una polera")
+} else if (producto == 2){
+console.log("El producto es una gorra")
+} else if (producto == 3){
+console.log("El producto es una remera")
+} else if (producto == 4){
+console.log("El producto es un pantalon")
+} else if(producto == 5) {
+console.log("El producto son zapatillas")
+} else {
+	alert("El producto no está en el sistema")
+} 
