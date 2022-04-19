ee3a8cdae2a25be5f89a02bd6f3027a1e1e2e17d
belen di leva <alexiabelengonzalezdileva@gmail.com>
2022-04-18 23:38:13 -0300

    AGREGO FUNCTION

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
-let producto = parseInt(prompt("Ingrese código del producto"))
-	if (producto == 1) {
-console.log("El producto es una polera")
-} else if (producto == 2){
-console.log("El producto es una gorra")
-} else if (producto == 3){
-console.log("El producto es una remera")
-} else if (producto == 4){
-console.log("El producto es un pantalon")
-} else if(producto == 5) {
-console.log("El producto son zapatillas")
-} else {
-	alert("El producto no está en el sistema")
-} 
+let monto= parseInt(prompt('ingrese el monto'))
+let cuotas= parseInt(prompt('ingrese cantidad de cuotas (pueden ser desde 1 a 12)'))
+let pagoTotal=0
+function pagoCuotas(monto,cuotas){
+    if (cuotas<=6){
+        pagoTotal=monto/cuotas;
+        alert('No tiene recargo, por lo tanto, el monto a abonar en '+cuotas+' meses es de '+pagoTotal+'.')
+    }else if (cuotas<=12){
+        pagoTotal=(monto/cuotas)*1.1;
+        alert('Tiene un recargo del 10%. El monto a abonar en '+cuotas+' meses es de '+pagoTotal+'.')
+    }else{
+        alert('Ingrese cantidad de cuotas válido')
+    }
+}
+pagoCuotas(monto,cuotas);
